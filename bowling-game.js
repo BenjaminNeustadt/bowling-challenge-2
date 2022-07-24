a6a7a7d2225f4ad1fdf3b922e6688709b3ae1b99
benjamin <b.james.neustadt@gmail.com>
2022-07-24 23:03:05 +0200

    Implemented logic for a strike, before checking for spare; we need to increment the rollIndex by 1 instead of 2 because we go directly to next frame; then we continue; jest now passes

--- bowling-game.js
+++ bowling-game.js
@@ -13,6 +13,11 @@ class BowlingGame {
     let rollIndex = 0;
 
     for (let frameIndex = 0; frameIndex < 10; frameIndex++) { 
+      if (this.rolls[rollIndex] === 10) {
+        score += 10 + this.rolls[rollIndex  + 1] + this.rolls[rollIndex + 2];
+        rollIndex++;
+        continue;
+      }
       const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
 
       if (this.isSpare(frameScore)) {
