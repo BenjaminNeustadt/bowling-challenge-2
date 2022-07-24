39dd07ef37691879a83944be866cc2ebfba2a80d
benjamin <b.james.neustadt@gmail.com>
2022-07-24 19:09:12 +0200

    Deleted commented out multiple rolls inside two first tests
    jest still passes

--- bowling-game.test.js
+++ bowling-game.test.js
@@ -7,9 +7,6 @@ beforeEach(() => {
 // -/- -/- -/- -/- -/- -/- -/- -/- -/- -/- = 0 (Gutter Game)
 
 it('should return 0 for a game of all zeros', () => {
-  // for (let i = 0; i<20; i++) {
-  //   game.roll(0);
-  // }
   rollMany(0, 20);
 
   expect(game.score).toEqual(0);
@@ -18,12 +15,8 @@ it('should return 0 for a game of all zeros', () => {
 // 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1 = 20 
 
 it('should return 20 for a game of all ones', () => {
-  // for (let i = 0; i<20; i++) {
-  //   game.roll(1);
-  // }
-
   rollMany(1, 20);
-
+  
   expect(game.score).toEqual(20);
 });
 
