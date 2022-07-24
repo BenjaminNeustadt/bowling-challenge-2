eb05913f56e238bfa704297eb1516d9f1c8768ea
benjamin <b.james.neustadt@gmail.com>
2022-07-24 18:13:32 +0200

    created 2nd test for a game of all ones; jest fails as returns 0 but should return 20

--- bowling-game.test.js
+++ bowling-game.test.js
@@ -1,5 +1,7 @@
 const BowlingGame = require('./bowling-game.js');
 
+// -/- -/- -/- -/- -/- -/- -/- -/- -/- -/- = 0 (Gutter Game)
+
 it('should return 0 for a game of all zeros', () => {
   const game = new BowlingGame();
   for (let i = 0; i<20; i++) {
@@ -7,4 +9,15 @@ it('should return 0 for a game of all zeros', () => {
   }
 
   expect(game.score).toEqual(0);
+})
+
+// 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1 = 20 
+
+it('should return 20 for a game of all ones', () => {
+  const game = new BowlingGame();
+  for (let i = 0; i<20; i++) {
+    game.roll(1);
+  }
+
+  expect(game.score).toEqual(20);
 })
