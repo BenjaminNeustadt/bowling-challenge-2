3ae43c87f7adb0a2e8fa81ed56a9f6130a9b1346
benjamin <b.james.neustadt@gmail.com>
2022-07-24 19:08:13 +0200

    Refactored test; creating function that rolls multiple times with the given number of pins knocked down for each roll

--- bowling-game.test.js
+++ bowling-game.test.js
@@ -7,9 +7,10 @@ beforeEach(() => {
 // -/- -/- -/- -/- -/- -/- -/- -/- -/- -/- = 0 (Gutter Game)
 
 it('should return 0 for a game of all zeros', () => {
-  for (let i = 0; i<20; i++) {
-    game.roll(0);
-  }
+  // for (let i = 0; i<20; i++) {
+  //   game.roll(0);
+  // }
+  rollMany(0, 20);
 
   expect(game.score).toEqual(0);
 });
@@ -17,9 +18,17 @@ it('should return 0 for a game of all zeros', () => {
 // 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1 1/1 = 20 
 
 it('should return 20 for a game of all ones', () => {
-  for (let i = 0; i<20; i++) {
-    game.roll(1);
-  }
+  // for (let i = 0; i<20; i++) {
+  //   game.roll(1);
+  // }
+
+  rollMany(1, 20);
 
   expect(game.score).toEqual(20);
 });
+
+function rollMany(pins, rolls) {
+  for (let i = 0; i < rolls; i++) {
+    game.roll(pins);
+  }
+}
